1061d9be8a3524b21e71d09ba27db995719342f7
IsMo167 <ismo167.est@gmail.com>
2024-09-13 17:31:08 -0600

    Se agregó los cambios realizados en el laboratorio del 13 sep

--- Laboratorio 2/JuegoBatalla.java
+++ Laboratorio 2/JuegoBatalla.java
@@ -6,60 +6,28 @@ public class JuegoBatalla{
         
         String nombreRobot;
         int puntosVidaRobot;
-        int ataqueRobot;
+        int ataqueMaxRobot;
         int defensaRobot;
+         
         
-        System.out.println ("Bienveni@s al juego Batalla de Robots");
-        System.out.println ("Favor antes de empezar, selecciona las características de cada robot");
-        System.out.println ("-----------------------------------------------------------------------");
-
-        System.out.print ("Ingrese el nombre nombre del robot 1: ");
-        nombreRobot = scanner.nextLine ();
-        System.out.print ("Ingrese los puntos de vida del robot 1 (50-100): ");
-        puntosVidaRobot = scanner.nextInt ();
-        System.out.print ("Ingrese la fuerza del ataque del robot 1 (10-20): ");
-        ataqueRobot = scanner.nextInt ();
-        System.out.print ("Ingrese la defensa del robot 1 (1-10): ");
-        defensaRobot = scanner.nextInt ();
-        Robot robot1 = new Robot (nombreRobot,puntosVidaRobot,ataqueRobot,defensaRobot);
-        System.out.println ("-----------------------------------------------------------------------");
-
-        System.out.print ("Ingrese el nombre nombre del robot 2: ");
-        nombreRobot = scanner.nextLine ();
-        System.out.print ("Ingrese los puntos de vida del robot 2 (50-100): ");
-        puntosVidaRobot = scanner.nextInt ();
-        System.out.print ("Ingrese la fuerza del ataque del robot 2 (10-20): ");
-        ataqueRobot = scanner.nextInt ();
-        System.out.print ("Ingrese la defensa del robot 2 (1-10): ");
-        defensaRobot = scanner.nextInt ();
-        Robot robot2 = new Robot (nombreRobot,puntosVidaRobot,ataqueRobot,defensaRobot);
-        System.out.println ("-----------------------------------------------------------------------");
-
-        System.out.print ("Ingrese el nombre nombre del robot 3: ");
-        nombreRobot = scanner.nextLine ();
-        System.out.print ("Ingrese los puntos de vida del robot 3 (50-100): ");
-        puntosVidaRobot = scanner.nextInt ();
-        System.out.print ("Ingrese la fuerza del ataque del robot 3 (10-20): ");
-        ataqueRobot = scanner.nextInt ();
-        System.out.print ("Ingrese la defensa del robot 3 (1-10): ");
-        defensaRobot = scanner.nextInt ();
-        Robot robot3 = new Robot (nombreRobot,puntosVidaRobot,ataqueRobot,defensaRobot);
-        System.out.println ("-----------------------------------------------------------------------");
-
+        
+        
+        
+        
         int opcion;
         do{
-            System.out.print("------------Menú Batalla------------");
-            System.out.print("1. Mostrar estado Robot");
-            System.out.print("2. Atacar Robot");
-            System.out.print("3. Defender Robot");
-            System.out.print("4. Detener Juego");
+            System.out.println("------------Menú Batalla------------");
+            System.out.println("1. Iniciar Batalla Robot");
+            System.out.println("2. Atacar Robot");
+            System.out.println("3. Defender Robot");
+            System.out.println("4. Detener Juego");
             opcion = scanner.nextInt();
             
             switch (opcion){
                 case 1:
-                    mostrarEstadoRobot (seleccionarRobot (scanner, robot1, robot2,robot3));
+                    iniciarBatalla();
                     break;
-                case 2:
+                /*case 2:
                     atacarRobot (scanner, seleccionarRobot (scanner, robot1, robot2,robot3));
                     break;
                 //case 3:
@@ -67,13 +35,39 @@ public class JuegoBatalla{
                    // break;
                 case 4:
                     System.out.println ("Saliendo del Juego...");
-                    break;
+                    break;*/
                 default:
                     System.out.println ("Opción inválida. Por Favor selecciones una opción válida");
             }
         } while (opcion != 4);
     }
-    
+    private static void iniciarBatalla(){
+        Scanner scanner = new Scanner (System.in);
+        System.out.print ("Ingrese la cantidad de robots: ");
+        int cantidadRobot = scanner.nextInt ();
+        scanner.nextLine();    
+        
+        Robot [] robotArray = new Robot [cantidadRobot];
+        
+        for (int i = 0; i <= cantidadRobot; i++){
+            System.out.print ("Ingrese el nombre del robot: ");
+            String nombreRobot = scanner.nextLine (); 
+            System.out.print ("Ingrese los puntos de vida: ");
+            int puntosVidaRobot = scanner.nextInt ();
+            System.out.print ("Ingrese un valor de ataque máximo (de 10 a 20): ");
+            int ataqueMaxRobot = scanner.nextInt ();
+            while (ataqueMaxRobot < 10 || 20 < ataqueMaxRobot) {    
+            System.out.print ("VALOR INCORRECTO: Ingrese nuevamente el valor de ataque máximo (de 10 a 20): ");
+            ataqueMaxRobot = scanner.nextInt ();      
+            }
+            scanner.nextLine();
+            
+            robotArray[i] = new Robot(nombreRobot,puntosVidaRobot, ataqueMaxRobot);
+            
+        }
+        
+    }
+    /*
     private static Robot seleccionarRobot (Scanner scanner, Robot robot1, Robot robot2, Robot robot3){
         System.out.print ("Ingrese el número de robot (1-3)");
         int numRobot = scanner.nextInt ();
@@ -99,6 +93,6 @@ public class JuegoBatalla{
     public static void atacarRobot (Scanner scanner, Robot robot){
         if (robot != null){
             
-        }
-    }
+    }*/
+
 }
--- Laboratorio 2/Robot.java
+++ Laboratorio 2/Robot.java
@@ -7,77 +7,29 @@ public class Robot{
     
     private String nombre;
     private int puntosVida;
-    private int ataque;
+    private int ataqueMax;
     
     private int defensa;
     
-    public Robot (String nombre, int puntosVida, int ataque, int defensa){
+    public Robot (String nombre, int puntosVida, int ataqueMax){
         this.nombre = nombre;
-        if (puntosVida >= 50 && 100 >= puntosVida){
-            this.puntosVida = puntosVida;
-        }
-        else if (puntosVida > 100) {
-            this.puntosVida = 100;
-        }
-        else if (50 > puntosVida) {
-            this.puntosVida = 50;
-        }
-        if (ataque >= 10 && 20 >= ataque){
-            this.ataque = ataque;
-        }
-        else if (ataque > 20){
-            this.ataque = 20;
-        }
-        else if (10 > ataque){
-            this.ataque = 10;
-        }
-        if (10 >= defensa && defensa > 0){
-            this.defensa = defensa;
-        }
-        else if (10 > defensa){
-            this.defensa = 10;
-        }
-        else if (0 > defensa){
-            this.defensa = 1;
-        }
+        this.puntosVida = puntosVida;
+        this.ataqueMax = ataqueMax;
     }
     
     public void setNombre (String nombre){
         this.nombre = nombre;
     }
     public void setPuntosVida (int puntosVida){
-        if (puntosVida >= 50 && 100 >= puntosVida){
-            this.puntosVida = puntosVida;
-        }
-        else if (puntosVida > 100) {
-            this.puntosVida = 100;
-        }
-        else if (50 > puntosVida) {
-            this.puntosVida = 50;
-        }
+        this.puntosVida = puntosVida;
     }
-    public void setAtaque (int ataque){
-        if (ataque >= 10 && 20 >= ataque){
-            this.ataque = ataque;
-        }
-        else if (ataque > 20){
-            this.ataque = 20;
-        }
-        else if (10 > ataque){
-            this.ataque = 10;
-        }
+    public void setAtaqueMax (int ataqueMax){
+        this.ataqueMax = ataqueMax;
     }
     
     public void setDefensa (int defensa){
-        if (10 >= defensa && defensa > 0){
-            this.defensa = defensa;
-        }
-        else if (10 > defensa){
-            this.defensa = 10;
-        }
-        else if (0 > defensa){
-            this.defensa = 1;
-        }
+        this.defensa = defensa;
+        
     }
     
     public String getNombre (){
@@ -86,8 +38,8 @@ public class Robot{
     public int getPuntosVida (){
         return puntosVida;
     }
-    public int getAtaque (){
-        return ataque;
+    public int getAtaqueMax (){
+        return ataqueMax;
     }
     
     public void atacar (Robot otroRobot){
