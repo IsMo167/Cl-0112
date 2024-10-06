75f8a39cf7068babe499f8e0224e73e2852b0a8b
IsMo167 <167446469+IsMo167@users.noreply.github.com>
2024-10-06 10:16:11 -0600

    Se añaden las clases en que se trabajarán de Cuatro en línea y Controlador

---
+++ Proyecto Programado 1/Proyecto Programado 1/CuatroEnLinea.java
@@ -0,0 +1,44 @@
+
+public class CuatroEnLinea{
+    char [][] tablero;
+    int jugadorActual;
+    
+    public CuatroEnLinea (char [][] tablero,int jugadorActual ){
+        this.tablero = tablero;
+        this.jugadorActual = jugadorActual;
+    }
+    public void setTablero (char [][] tablero){
+        this.tablero = tablero;
+    }
+    public void setJugadorActual (int jugadorActual){
+        this.jugadorActual = jugadorActual;
+    }
+    public char [][] getTablero (){
+        return tablero;
+    }
+    public int getJugadorActual (){
+        return jugadorActual;
+    }
+    
+    public void iniciarJugador (){
+        
+    }
+    public void hacerMovimiento (int columna){
+        
+    }
+    public void mostrarTablero (){
+        
+    }
+    public void esJuegoTerminado (){
+        
+    }
+    public void esGanador (){
+        
+    }
+    public void esEmpate (){
+        
+    }
+    public void cambiarJugador (){
+        
+    }
+}
