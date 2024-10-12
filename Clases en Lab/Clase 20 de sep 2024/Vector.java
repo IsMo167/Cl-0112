2537cf62c335003946d1697edf68d1e8be169323
IsMo167 <ismo167.est@gmail.com>
2024-10-12 11:37:24 -0600

    Cambios insignificantes

--- Clases en Lab/Clase 20 de sep 2024/Vector.java
+++ Clases en Lab/Clase 20 de sep 2024/Vector.java
@@ -2,9 +2,7 @@ import java.util.Random;
 public class Vector{
     Random randomGenerador = new Random ();
     int [] vector;
-    
-    int i;    
-    int j;
+
     
     public Vector(int [] vector){
         this.vector = vector;
@@ -16,14 +14,14 @@ public class Vector{
     
     public void imprimirVector (){
         System.out.print("Los valores del vector son: ");
-        for (i = 0; vector.length > i; i++){
+        for (int i = 0; vector.length > i; i++){
             System.out.print(" " + vector [i] + " ");
         }
         System.out.println ("");
     }
     
     public void cambiarVector (){
-        for (i = 0; vector.length > i; i++){
+        for (int i = 0; vector.length > i; i++){
             this.vector[i] = randomGenerador.nextInt(101);
         }
     }
@@ -31,10 +29,14 @@ public class Vector{
         int posicionMenor = 0;
         int valorMenor = vector [posicionMenor];
         
-        for (i = 0; vector.length > i; i++){
-            for (j = i; vector.length > j; j++){
-                
+        for (int i = 0; vector.length > i; i++){
+            for (int j = 0; vector.length > j; j++){
+                if (> vector[i]  ){
+
+                }
             }
+            
+            vector [posicion]
         }
     }
 }
