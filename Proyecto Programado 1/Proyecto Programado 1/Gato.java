4ab7eaa8607ab9ccbfd0ca1390b3e32a09da37cf
SebasChaves6 <sebaschaves2901@gmail.com>
2024-10-12 23:15:23 -0600

    Logica y prueba de metodo de poner ficha Gato

--- Proyecto Programado 1/Proyecto Programado 1/Gato.java
+++ Proyecto Programado 1/Proyecto Programado 1/Gato.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Gato{
      char [] [] tablero = new char[3][3];
+     char jugadorActual;
 
     //recorre el tablero vacio y lo llena con "_", para mostrarlo al usuario
      public void llenarTablero(){
@@ -20,17 +22,33 @@ public class Gato{
      }
      //recibe la fila y la columna y sustituye ese espacio por el char del jugador
      public void movimiento(int fila, int columna){
-
+        if(tablero[fila][columna] == '_'){
+            tablero[fila][columna] = 'X';
+            imprimirTablero();
+        } else {
+            System.out.println("Espacio ocupado");
+        }
      }
 
      public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Gato gato1 = new Gato();
-
+        boolean enJuego = true;
         gato1.llenarTablero();
         gato1.imprimirTablero();
-        
 
-
+        while(enJuego){
+            System.out.println("Ingrese la fila y la columna en la que desea poner su ficha: ");
+            int fila = scanner.nextInt();
+            int columna = scanner.nextInt();
+            gato1.movimiento(fila-1, columna-1);
+            System.out.println("Desea seguir jugando? 1 = Si   2 = No");
+            int desicion = scanner.nextInt();
+            if (desicion==2) {
+                enJuego = false;
+            }
+        }
+        scanner.close();
      }
 
 }
