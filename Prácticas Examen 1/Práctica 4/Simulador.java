7274aad6eef4b97c00195ed4b948e7670fb6bd2a
IsMo167 <167446469+IsMo167@users.noreply.github.com>
2024-10-18 20:42:22 -0600

    Se suben las prácticas de exámen

--- Prácticas Examen 1/Práctica 4/Simulador.java
+++ Prácticas Examen 1/Práctica 4/Simulador.java
@@ -3,25 +3,30 @@ import java.util.Scanner;
 public class Simulador{
     public static void main (String [] args){
         Scanner scanner = new Scanner (System.in);
-        Empleado [] empleadoArreglo = new Empleado [5];
+        Empleado [] empleadoArreglo = new Empleado [3];
         
         int id_EmpleadoMain;
         String nombreMain;
         float salarioMain;
         
         for (int i = 0; empleadoArreglo.length > i; i++){
-            System.out.println ("Ingrese el id (utilizar números del 1 al 5) del empleado : ");
+            System.out.print("Ingrese el id (utilizar números del 1 al 3) del empleado : ");
             id_EmpleadoMain = scanner.nextInt();
-            
-            System.out.println ("Ingrese el nombre del empleado : ");
+            scanner.nextLine();
+            System.out.print ("Ingrese el nombre del empleado : ");
             nombreMain = scanner.nextLine();
-            
-            System.out.println ("Ingrese el salario del empleado: ");
+            System.out.print ("Ingrese el salario del empleado: ");
             salarioMain = scanner.nextFloat();
+            scanner.nextLine();
+            empleadoArreglo[i] = new Empleado(id_EmpleadoMain, nombreMain, salarioMain);
         }
         
-        
+        System.out.println("\nDetalles de los empleados:");
+        for (Empleado empleado : empleadoArreglo) {
+            empleado.mostrar_Detalles();
+        }
         
         
     }
 }
+
